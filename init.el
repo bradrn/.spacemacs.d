a15201020a4736765c2a7a410de895707f6aa12b
Brad Neimann <brad.neimann@hotmail.com>
2017-10-23 11:26:48 +1100

    Add zenburn to dotspacemacs-themes

--- init.el
+++ init.el
@@ -145,6 +145,7 @@ values."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(solarized-dark
+                         zenburn
                          spacemacs-dark
                          monochrome)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
