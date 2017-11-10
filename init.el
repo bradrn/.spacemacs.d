4228660cae691f4f14707f166dd5a6521cb1cd86
Brad Neimann <brad.neimann@hotmail.com>
2017-11-10 14:11:38 +1100

    Fix SumatraPDF integration with spaces

--- init.el
+++ init.el
@@ -377,8 +377,7 @@ you should place your code here."
   (setq TeX-source-correlate-mode t)
   (setq TeX-source-correlate-method 'synctex)
   (setq TeX-view-program-list
-  '(("Sumatra PDF" ("\"C:/Program Files/SumatraPDF/SumatraPDF.exe\" -reuse-instance"
-     (mode-io-correlate " -forward-search %b %n ") " %o"))))
+  '(("Sumatra PDF" ("\"C:/Program Files/SumatraPDF/SumatraPDF.exe\" -reuse-instance" (mode-io-correlate " -forward-search \"%b\" %n ") " %o"))))
 
   (eval-after-load 'tex
    '(progn
