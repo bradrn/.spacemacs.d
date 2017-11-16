75f1b09a39bb23e6c815ca8c0ef93bfafa7f3441
Brad Neimann <brad.neimann@hotmail.com>
2017-11-16 17:25:03 +1100

    Set font-latex-verbatim-face to normal font

--- init.el
+++ init.el
@@ -479,4 +479,4 @@ you should place your code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-latex-verbatim-face ((t nil))))
+ '(font-latex-verbatim-face ((t (:inherit nil :slant normal)))))
